927de272ffba38bb8778d85d126f31ae02d40186
skreider-ce <skreider@corevitas.com>
2024-01-25 13:48:25 -0500

    cleaning

--- R/hello.R
+++
@@ -1,7 +0,0 @@
-#' @export
-hello <- function() {
-  print("Hello, world!")
-}
-
-
-# staticFolderUrl <- "C:/Users/ScottKreider/Corrona LLC/Biostat and Epi Team Site - Registry Data QC Checks/registryCheckStorage/ad"
