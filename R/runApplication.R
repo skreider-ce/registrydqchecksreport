cb1f978928733d83562f7bc0ff81f6bb92d09ed7
skreider-ce <skreider@corevitas.com>
2024-01-25 09:03:23 -0500

    url Input
    regdqchecks::runApplication(outpUrl) now takes an output destination folder url (outpUrl) as an input to the function.
    
    This allows the runApplication function to point at a specific folder that houses the critical check output files and run the app with whatever files are in that location

--- R/runApplication.R
+++ R/runApplication.R
@@ -1,5 +1,8 @@
 #' @export
-runApplication <- function(){
+runApplication <- function(outputUrl){
+
+  .GlobalEnv$.outputUrl = outputUrl
+
   appDir <- system.file("myapp", package = "regdqchecks")
   if (appDir == "") {
     stop("Could not find myapp. Try re-installing `regdqchecks`.", call. = FALSE)
@@ -7,3 +10,11 @@ runApplication <- function(){
 
   shiny::runApp(appDir, display.mode = "normal")
 }
+
+
+# outputUrl <- "C:/Users/ScottKreider/Corrona LLC/Biostat and Epi Team Site - Registry Data QC Checks/registryCheckStorage/ad"
+
+
+# regdqchecks::runApplication(outputUrl = "C:/Users/ScottKreider/Corrona LLC/Biostat and Epi Team Site - Registry Data QC Checks/registryCheckStorage/ad")
+# regdqchecks::runApplication(outputUrl = "C:/Users/ScottKreider/Corrona LLC/Biostat and Epi Team Site - Registry Data QC Checks/registryCheckStorage/ms")
+# regdqchecks::runApplication(outputUrl = "C:/Users/ScottKreider/Corrona LLC/Biostat and Epi Team Site - Registry Data QC Checks/registryCheckStorage/pso")
--- inst/myapp/ui.R
+++ inst/myapp/ui.R
@@ -1,20 +1,22 @@
 # Define the UI
-ui <- shinydashboard::dashboardPage(
-  shinydashboard::dashboardHeader(title = "Critical Checks"),
-  shinydashboard::dashboardSidebar(
-    shiny::fluidRow(
-      # Dropdown for main list items
-      selectInput("dataPullDate", "Choose a Data Pull Date", choices = NULL)
-    )
-    ,shiny::fluidRow(
-      shiny::selectInput("datasetName", "Choose a Dataset", choices = NULL)
-    )
+ui <- function(){
+    shinydashboard::dashboardPage(
+    shinydashboard::dashboardHeader(title = "Critical Checks"),
+    shinydashboard::dashboardSidebar(
+      shiny::fluidRow(
+        # Dropdown for main list items
+        selectInput("dataPullDate", "Choose a Data Pull Date", choices = NULL)
+      )
+      ,shiny::fluidRow(
+        shiny::selectInput("datasetName", "Choose a Dataset", choices = NULL)
+      )
 
-  ),
-  shinydashboard::dashboardBody(
-    shiny::tags$script(HTML("$('body').addClass('fixed');"))
-    ,shiny::actionButton("downloadBtn", "Download as PDF")
-    ,shiny::fluidRow(uiOutput("totalCritCheckPlaceholder"))
-    ,shiny::fluidRow(uiOutput("criticalCheckOutputPlaceholder"))
+    ),
+    shinydashboard::dashboardBody(
+      shiny::tags$script(HTML("$('body').addClass('fixed');"))
+      ,shiny::actionButton("downloadBtn", "Download as PDF")
+      ,shiny::fluidRow(uiOutput("totalCritCheckPlaceholder"))
+      ,shiny::fluidRow(uiOutput("criticalCheckOutputPlaceholder"))
+    )
   )
-)
+}
